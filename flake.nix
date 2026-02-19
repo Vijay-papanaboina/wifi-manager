359a4350eb0d76da8e27f31b4ec35fc3d5781217
Vijay-papanaboina <vijaypapanaboina3@gmail.com>
2026-02-19 14:40:25 +0530

    feat: add nix support via flakes

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  description = "A lightweight WiFi manager for Wayland compositors";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, crane, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        # System dependencies
+        buildInputs = with pkgs; [
+          gtk4
+          gtk4-layer-shell
+          networkmanager # for libnm
+          glib
+          cairo
+          pango
+          gdk-pixbuf
+        ];
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          wrapGAppsHook4
+        ];
+
+        commonArgs = {
+          src = pkgs.lib.cleanSource ./.;
+          strictDeps = true;
+
+          inherit buildInputs nativeBuildInputs;
+        };
+
+        wifi-manager = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
+      in
+      {
+        packages.default = wifi-manager;
+
+        devShells.default = craneLib.devShell {
+          packages = buildInputs ++ nativeBuildInputs ++ (with pkgs; [
+            rustToolchain
+            clippy
+            rustfmt
+          ]);
+
+          shellHook = ''
+            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
+          '';
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      }
+    );
+}
